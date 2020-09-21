49adb47645281a3de6099b3164625d7ce66ef2b2
STBoyden <stboyden@stboyden.xyz>
2020-09-21 22:44:36 +0100

    Add required types
    - Add Language enum to specify the language of the project.
    - Add Project struct to specify the details of a certain C/C++ project.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use serde_derive::*;
+use std::collections::HashMap;
+use std::fmt;
+
+#[derive(Deserialize, Serialize)]
+enum Language {
+    C,
+    CXX,
+}
+
+impl fmt::Display for Language {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let lang = match self {
+            Language::C => "C",
+            Language::CXX => "C++",
+        };
+
+        write!(f, "{}", lang)
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+struct Project {
+    name: String,
+    language: Language,
+    directories: HashMap<String, String>,
+}
+
 fn main() {
 
 }
