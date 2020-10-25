0c9ab92f0300ff95cba327a97a93848601c23d03
STBoyden <stboyden@stboyden.xyz>
2020-10-25 22:05:54 +0000

    Add SFML example

---
+++ examples/README.md
@@ -0,0 +1,5 @@
+# Examples
+
+All these examples are made to be compiled and ran on a Linux x86_64 system. If
+you want to compile and run for another OS or architecture, please make the
+necessary changes to the Ocean.toml.
---
+++ examples/sfml-test/src/main.cpp
@@ -0,0 +1,22 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(200, 200), "Ocean with SFML");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
