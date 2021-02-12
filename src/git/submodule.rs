1e239457777b679da1c789d0de56d8d5dfb42316
STBoyden <stboyden@stboyden.xyz>
2021-02-12 23:48:50 +0000

    Implement Submodules::add_submodule (#5)

--- src/git/submodule.rs
+++ src/git/submodule.rs
@@ -1,4 +1,12 @@
+use std::{
+    fs::create_dir,
+    path::Path,
+    process::{self, Stdio},
+};
+use url::Url;
+
 fn default_path() -> String { "third_party".into() }
+fn default_branch() -> String { "master".into() }
 
 #[derive(Serialize, Deserialize)]
 pub struct Submodules {
@@ -8,7 +16,58 @@ pub struct Submodules {
     pub submodules: Vec<Submodule>,
 }
 
-    // TODO(#5) Submodule add
+impl Submodules {
+    // TODO(#5) Submodules update
+    // TODO(#5) Submodules remove
+    // TODO(#5) Submodules build
+    pub fn add_submodule<S: Into<String>>(
+        &mut self,
+        origin: S,
+        branch: Option<String>,
+        directory_name: Option<String>,
+    ) {
+        let branch = branch.unwrap_or_else(default_branch);
+        let origin: String = origin.into();
+
+        if !Path::new(&self.path).exists() {
+            create_dir(&self.path).expect(&format!(
+                "Could not create '{}' submodule directory",
+                self.path
+            ));
+        }
+
+        self.submodules
+            .push(Submodule::new(origin.clone(), branch.clone()));
+
+        let mut command = process::Command::new("git");
+        command.args(&["submodule", "add", origin.as_str(), "-b", branch.as_str()]);
+
+        if let Some(directory_name) = directory_name {
+            command.arg(format!("{}/{}", self.path, directory_name));
+        } else {
+            let directory_name = &Url::parse(&origin)
+                .expect(&format!("Could not parse '{}' as URL", origin))
+                .path_segments()
+                .expect(&format!(
+                    "Could not get path segments of '{}'. Are you sure you entered a \
+                     Git repository?",
+                    origin
+                ))
+                .map(|x| x.to_owned())
+                .collect::<Vec<String>>()[0];
+
+            command.arg(format!("{}/{}", default_path(), directory_name));
+        }
+
+        command
+            .stderr(Stdio::null())
+            .spawn()
+            .expect("Could not spawn Git")
+            .wait()
+            .expect("Git did not run");
+    }
+}
+
 impl Default for Submodules {
     fn default() -> Self {
         Self {
@@ -18,23 +77,18 @@ impl Default for Submodules {
     }
 }
 
-fn default_branch() -> String { "master".into() }
-
 #[derive(Serialize, Deserialize)]
 pub struct Submodule {
-    pub origin: String,
+    origin: String,
     #[serde(default = "default_branch")]
-    pub branch: String,
-    #[serde(default)]
-    pub tag: Option<String>,
+    branch: String,
 }
 
 impl Submodule {
-    pub fn new<S: Into<String>>(origin: S, branch: S, tag: Option<String>) -> Self {
+    pub fn new<S: Into<String>>(origin: S, branch: S) -> Self {
         Self {
             origin: origin.into(),
             branch: branch.into(),
-            tag,
         }
     }
 
